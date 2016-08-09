fbce56df6c3793bba6f32251f9e7d6dd1742db29
Marcio Lima <marcimpriori@gmail.com>
2016-08-09 19:03:39 -0300

    Alteração da mensagem de retorno na autenticação.

--- Smart.Dimdim/Smart.Dimdim.Api/App_Start/BasicAuthApiFilterAttribute.cs
+++ Smart.Dimdim/Smart.Dimdim.Api/App_Start/BasicAuthApiFilterAttribute.cs
@@ -4,10 +4,10 @@ using Smart.Dimdim.Api.Models;
 using System;
 using System.Net;
 using System.Net.Http;
-using System.Net.Http.Headers;
+
 using System.Text;
-using System.Web;
 using System.Web.Http.Filters;
+using System.Web.Http.OData.Extensions;
 
 namespace Smart.Dimdim.Api.App_Start
 {
@@ -15,12 +15,10 @@ namespace Smart.Dimdim.Api.App_Start
     {
         public override void OnActionExecuting(System.Web.Http.Controllers.HttpActionContext actionContext)
         {
-            //try
-            //{
+            try
+            {
                 if (actionContext.Request.Headers.Authorization == null)
-                    throw new ODataErrorException(
-                        "Cabeçalho", new HttpApiException(HttpStatusCode.Unauthorized));
-                    //throw new HttpApiException(HttpStatusCode.Unauthorized, "Cabeçalho 'Authorization' não encontrado.");
+                    throw new HttpApiException(HttpStatusCode.Unauthorized, "Cabeçalho 'Authorization' não encontrado.");
 
                 string authToken = actionContext.Request.Headers.Authorization.Scheme;
                 string decodedToken = Encoding.UTF8.GetString(Convert.FromBase64String(authToken));
@@ -31,14 +29,17 @@ namespace Smart.Dimdim.Api.App_Start
 
                 new Token().Login(email, password);
                 base.OnActionExecuting(actionContext);
-            //}
-            //catch (HttpApiException ex)
-            //{
-            //    actionContext.Response = new HttpResponseMessage(ex.StatusCode)
-            //    {
-
-            //    };
-            //}
+            }
+            catch (HttpApiException ex)
+            {
+                var odata = new ODataError
+                    {
+                        Message = ex.Message,
+                        InnerError = new ODataInnerError(ex)
+                    };
+                actionContext.Response =
+                    actionContext.Request.CreateErrorResponse(ex.StatusCode, odata);
+            }
 
         }
     }
